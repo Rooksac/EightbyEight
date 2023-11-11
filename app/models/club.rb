47c9bdad7f4bc70b5086d6c62879efb527837a8e
Zak <Zakedoubled@gmail.com>
2023-11-11 12:54:45 -0500

    added class methods for students and clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,17 @@
 class Club < ApplicationRecord
     belongs_to :instructor
     has_many :students
+
+    def top_3
+        top_3_students = students.sort_by { |student| -student.total_wins }.take(3)
+
+        top_3_students_with_wins = top_3_students.map do |student|
+            {
+              student: student.student_name,
+              wins: student.total_wins
+            }
+          end
+
+        top_3_students_with_wins
+    end
 end
