1f5612cd00379b61a3d8ba4623735b40f6e92006
Zak <Zakedoubled@gmail.com>
2023-11-13 20:30:39 -0500

    implements jwt

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem "active_model_serializers", "~> 0.10.12"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem 'jwt'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
  gem "rack-cors"
 
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,8 +1,20 @@
 class InstructorsController < ApplicationController
 
   before_action :find_instructor, only: [:show, :update, :destroy]
+  skip_before_action :authorized, only: [:create, :login]
 
-  
+  def login #for /login
+    #find by username from body
+    @user = Instructor.find_by(name: params[:name])
+    #check if user exists and password matches password digest
+    if (@user && @user.authenticate(params[:password]))
+        puts("user found")
+        #create token for front end
+        token = JWT.encode({user_id: @user.id}, secret_key, 'HS256')
+        #pass user instance and token to front end
+        render json: {user: InstructorSerializer.new(@user), token: token}
+    end 
+  end
 
   def create
     instructor = Instructor.create!(instructor_params)
@@ -11,6 +23,10 @@ class InstructorsController < ApplicationController
 
   def show
     render json: @instructor, include: ['clubs', 'clubs.students']
+  end
+
+  def me
+    render json: {user: @current_user}, status: :ok
   end
 
   def update
