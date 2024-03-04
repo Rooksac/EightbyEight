ec909773596b270fd15f591042c5c8426d2d77fa
logan-lampton <lampton.logan@gmail.com>
2024-03-04 11:02:25 -0500

    updated student win_rate_white and win_rate_black

---
+++ .history/app/controllers/application_controller_20240228143713.rb
@@ -0,0 +1,40 @@
+class ApplicationController < ActionController::API
+    before_action :authorized
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
+  
+  
+    #asks the question is user logged in
+    def logged_in_user
+      headers = request.headers['Authorization']
+      if(headers)
+          token = headers.split(' ')[1]
+          cur_id = JWT.decode(token, secret_key, true, algorithm: 'HS256')
+          @current_user = Instructor.find_by(id: cur_id[0]["user_id"])
+          @current_user
+      end 
+    end 
+  
+     #throws error if not logged in
+    def authorized 
+      puts "checking... #{logged_in_user}"
+      # !! converts a value to boolean
+      render json: { message: 'Please log in' }, status: :unauthorized unless !!logged_in_user
+    end 
+  
+    def secret_key
+      Rails.application.credentials.secret_key
+    end
+  
+    private
+   
+  
+    def not_found_response(exception)
+      render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
+  
+    def validation_error_response(exception)
+      render json: {error: exception.record.errors.full_messages}, status: :unprocessable_entity
+    end
+end
