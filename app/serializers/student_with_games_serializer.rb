74245a2bc50e389785ac9e7d9bfc61767b3fad85
Zak <Zakedoubled@gmail.com>
2024-06-08 12:09:31 -0400

    updated serializers

--- app/serializers/student_with_games_serializer.rb
+++ app/serializers/student_with_games_serializer.rb
@@ -1,3 +1,3 @@
 class StudentWithGamesSerializer < ActiveModel::Serializer
-  attributes :id, :student_name, :grade, :games, :win_rate, :win_rate_black, :win_rate_white, :total_wins, :num_white_wins, :num_black_wins
+  attributes :id, :student_name, :grade, :games, :win_rate, :win_rate_black, :win_rate_white, :total_wins, :num_white_wins, :num_black_wins, :average_grade
 end
